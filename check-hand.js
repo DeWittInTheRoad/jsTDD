b2d1bebf0d8335b217b3fff143e123049ec1ca8e
Brandon <dewittintheroad@gmail.com>
2018-11-17 12:16:14 -0500

    intial commit

---
+++ check-hand.js
@@ -0,0 +1,31 @@
+var checkHand = function (hand) {
+    if (checkStraightFlush(hand)){
+        return "straight flush";
+    }
+    if (checkFourOfKind(hand)){
+        return "four of a kind";
+    }
+    if (checkFullHouse(hand)){
+        return "full house";
+    }
+    if (checkFlush(hand)){
+        return "flush";
+    }
+    if (checkStraight(hand)){
+        return "straight";
+    }
+    if (checkThreeOfKind(hand)){
+        return "three of a kind";
+    }
+    if (checkTwoPair(hand)){
+        return "two pair";
+    }
+    if (checkPair(hand)){
+        return "pair";
+    }
+    else {
+        return "high card";
+    }
+};
+
+console.log(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']));
