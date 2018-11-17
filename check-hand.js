7f0a5c88352ab989a3a95b0e72e10928c17d3448
Brandon <dewittintheroad@gmail.com>
2018-11-17 15:31:38 -0500

    values from hand passed

--- check-hand.js
+++ check-hand.js
@@ -2,9 +2,7 @@ var checkHand = function() {
     return 'pair';
 };
 
-var assert = require ('wish');
-assert(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']) === 'pair');
-assert(checkHand(['3-H', '3-C', '3-D', '5-H', '2-H']) === 'three of a kind');
+
 
 // var checkHand = function (hand) {
 //     if (checkStraightFlush(hand)){
