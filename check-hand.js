df322c8078b47b54f8b991c4a6d61acdebccf037
Brandon <dewittintheroad@gmail.com>
2018-11-17 12:31:11 -0500

    Asserts added

--- check-hand.js
+++ check-hand.js
@@ -1,3 +1,7 @@
+var assert = require ('assert');
+assert(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']) === 'pair');
+assert(checkHand(['3-H', '3-C', '3-D', '5-H', '2-H']) === 'three of a kind');
+
 var checkHand = function (hand) {
     if (checkStraightFlush(hand)){
         return "straight flush";
